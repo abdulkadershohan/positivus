7658b6c654086f931f9f77ebeced0e08dddaabca
abdulkadershohan <abdulkadirshohan@gmail.com.com>
2024-08-23 16:55:25 +0600

    feat: Add CompanyLogo component for displaying company logos

--- apps/frontend/src/app/(public)/page.tsx
+++ apps/frontend/src/app/(public)/page.tsx
@@ -1,13 +1,51 @@
 import Banner from "@/components/banner/Index";
+import CompanyLogo from "@/components/company-logo";
 import { Container, Stack } from "@mui/material";
+const data = {
+  title: `Navigating the digital landscape for success`,
+  description: `Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.`,
+  banner: {
+    src: "/images/banner/banner.svg",
+    alt: "Hero",
+  },
+  company_logo: [
+    {
+      src: "/images/company-logo/logo1.svg",
+      alt: "Logo 1",
+    },
+    {
+      src: "/images/company-logo/logo2.svg",
+      alt: "Logo 2",
+    },
+    {
+      src: "/images/company-logo/logo3.svg",
+      alt: "Logo 3",
+    },
+    {
+      src: "/images/company-logo/logo4.svg",
+      alt: "Logo 4",
+    },
+    {
+      src: "/images/company-logo/logo5.svg",
+      alt: "Logo 5",
+    },
+    {
+      src: "/images/company-logo/logo6.svg",
+      alt: "Logo 6",
+    },
+  ],
 
+}
 export default function Page() {
   return (
-    <Container maxWidth='lg'>
+    <Container maxWidth='lg' >
       <Stack
         py={4}
+        spacing={4}
       >
         <Banner />
+        <CompanyLogo />
+
 
 
       </Stack>
---
+++ apps/frontend/src/components/company-logo/index.tsx
@@ -0,0 +1,52 @@
+import { Stack } from '@mui/material'
+import Image from 'next/image'
+
+const data = [
+    {
+        src: "/images/company-logo/logo1.svg",
+        alt: "Logo 1",
+    },
+    {
+        src: "/images/company-logo/logo2.svg",
+        alt: "Logo 2",
+    },
+    {
+        src: "/images/company-logo/logo3.svg",
+        alt: "Logo 3",
+    },
+    {
+        src: "/images/company-logo/logo4.svg",
+        alt: "Logo 4",
+    },
+    {
+        src: "/images/company-logo/logo5.svg",
+        alt: "Logo 5",
+    },
+    {
+        src: "/images/company-logo/logo6.svg",
+        alt: "Logo 6",
+    },
+]
+export default function CompanyLogo() {
+    return (
+        <Stack direction='row' gap={{
+            xs: 2,
+            md: 12
+        }}
+            flexWrap={'wrap'}
+        >
+            {
+                data.map((logo, index) => (
+                    <Image
+                        src={logo.src}
+                        alt={logo.alt}
+                        key={index}
+                        width={125}
+                        height={48}
+                    />
+                ))
+
+            }
+        </Stack>
+    )
+}
