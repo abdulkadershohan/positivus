65af32b899f53cfcd55d23161ffd7e0318e8db14
abdulkadershohan <abdulkadirshohan@gmail.com.com>
2024-08-23 14:24:41 +0600

    feat: Add IconBox component for displaying icons with labels

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,15 +1,20 @@
+import IconBox from "@/components/shared/IconBox";
 import { Stack, Typography } from "@mui/material";
 
 export default function Page() {
   return (
     <Stack
       sx={{
-        bgcolor: '#f00'
+        bgcolor: "background.paper",
       }}
     >
       <Typography>
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </Typography>
+      <IconBox
+        bgcolor="#f00"
+        iconColor="#fff"
+      />
     </Stack>
   )
 }
