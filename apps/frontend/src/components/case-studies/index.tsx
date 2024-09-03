21dcf4ddc18eb4c4223bf6be76a4072c006253ff
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2024-09-04 00:11:49 +0600

    refactor: Update CaseStudies component to add image carousel functionality with react-slick

--- apps/frontend/src/components/case-studies/index.tsx
+++ apps/frontend/src/components/case-studies/index.tsx
@@ -1,7 +1,11 @@
 'use client';
 import CallMadeIcon from '@mui/icons-material/CallMade';
-import { Container, Divider, Stack, Typography } from '@mui/material';
+import { Box, Container, Divider, Stack, Typography } from "@mui/material";
 import Link from 'next/link';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import Card from '../shared/Card';
 import TextWithBg from '../shared/TextWithBg';
 const data = [
@@ -19,6 +23,53 @@ const data = [
     }
 ]
 export default function CaseStudies() {
+    const slider = React.useRef<Slider | null>(null);
+    const settings = {
+        className: "center",
+        infinite: true,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        speed: 500,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                },
+            },
+
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    // dots: true
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 1,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+
+    };
     return (
         <Container maxWidth='lg' id='blog'>
             <Stack spacing={6}>
@@ -111,7 +162,75 @@ export default function CaseStudies() {
                     </Stack>
 
                 </Card>
+                <Stack
+                    sx={{
+                        display: {
+                            xs: 'flex',
+                            md: 'none'
+                        }
+                    }}
+                >
+                    <Slider {...settings} ref={slider}>
+                        {data.map((item, index) => (
+                            <Box key={item.id} >
+                                <Stack
+                                    sx={{
+                                        bgcolor: (theme) => theme.palette.info.main,
+                                        p: 4,
+                                        borderRadius: '45px',
+                                        mx: 2,
+                                    }}
+                                >
+                                    <Stack spacing={4}>
+                                        <Typography
+                                            color={(theme) => theme.palette.text.secondary}
+                                            fontSize={{
+                                                xs: 16,
+                                                md: 18,
+                                            }}
+                                            fontWeight={400}
+                                        >
+                                            {item.text}
+                                        </Typography>
+                                        <Stack
+                                            direction='row'
+                                            gap={2} alignItems='center'
+                                            sx={{
+                                                cursor: 'pointer',
+                                                textDecoration: 'none',
+                                                width: 'fit-content',
+                                            }}
+                                            component={Link}
+                                            href='#'
+                                        >
+                                            <Typography
+                                                color={(theme) => theme.palette.primary.main}
+                                                variant='body1'
+                                            >
+                                                Learn More
+                                            </Typography>
+                                            <CallMadeIcon
+                                                sx={{
+                                                    color: (theme) => theme.palette.primary.main,
+                                                }}
+                                            />
+                                        </Stack>
+                                    </Stack>
+                                    {index < data.length - 1 && (
+                                        <Divider
+                                            flexItem
+                                            orientation='vertical'
+                                            sx={{
+                                                marginLeft: 4,
+                                            }}
+                                        />
+                                    )}
+                                </Stack>
+                            </Box>
+                        ))}
+                    </Slider>
+                </Stack>
             </Stack>
-        </Container>
+        </Container >
     )
 }
