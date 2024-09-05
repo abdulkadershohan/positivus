eac764ec7f5c29943e636dd9664ed71377a6398b
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2024-09-05 10:32:51 +0600

    backend update

--- apps/backend/types/generated/components.d.ts
+++ apps/backend/types/generated/components.d.ts
@@ -1,5 +1,34 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface FormRadio extends Schema.Component {
+  collectionName: 'components_form_radios';
+  info: {
+    displayName: 'radio';
+    icon: '';
+  };
+  attributes: {
+    label: Attribute.String;
+    value: Attribute.Integer;
+  };
+}
+
+export interface FormInput extends Schema.Component {
+  collectionName: 'components_form_inputs';
+  info: {
+    displayName: 'Input';
+    description: '';
+  };
+  attributes: {
+    label: Attribute.String;
+    placeholder: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'form.radio': FormRadio;
+      'form.input': FormInput;
+    }
+  }
 }
