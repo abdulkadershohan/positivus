e2a02f0a4a68d58558d34320d0bdb25068639b71
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2024-09-09 21:54:13 +0600

    Refactor database configuration to use environment variables

--- apps/backend/config/database.ts
+++ apps/backend/config/database.ts
@@ -1,14 +1,17 @@
-// strapi-api/config/database.js
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '1516'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      // uncomment the following lines if you are using SSL or remote database
+      // ssl: {
+      //   rejectUnauthorized: env.bool('DATABASE_SSL', false),
+      // },
     },
     debug: false,
   },
